bf09e60725214f89f1ece08e38fb624d479b691c
2001brenda <138245937+2001brenda@users.noreply.github.com>
2023-08-10 13:24:07 -0500

    producto.py

--- producto.py
+++ producto.py
@@ -1,25 +1,17 @@
-def crearproductos(lista_productos):
-    producto = {}
-    flag=True
-    while flag:
-        try:
-            nombreproducto = input("Nombre del producto: ")
-            costoproducto = int(input("costo del producto: "))
-
-        except ValueError:
-            print("ALGO SALIO MAL INTENTELO OTRA VEZ")
-        else:
-            producto["nombre"]= nombreproducto
-            producto["costo"]= costoproducto
-            lista_productos.append(producto)
-            producto = {}
-            pregunta = input("desea agregar mas productos? si/no")
-            if str(pregunta) != "SI":
-                flag = False
-
-    return lista_productos
-
-def listarproductos(lista_productos):
-    print('N Producto | Precio')
-    for producto in lista_productos:
-        print(f"{producto.get('nombre')} | {producto.get('costo')}")
+class Producto:
+    def __init__(self, nombre, precio, stock):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+    
+    def info(self):
+        return f"{self.nombre} {self.stock}"
+    
+    def actualizar_stock(self, stock_vendida):
+        self.stock = self.stock - stock_vendida
+    
+    def get_nombre(self):
+        return self.nombre
+    
+    def get_info_completa(self):
+        return f"{ self.nombre } { self.precio } { self.stock }"
